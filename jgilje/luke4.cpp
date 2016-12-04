b28efbddc27efa3d9f16c4e8de5dfc503ee596b1
Joakim L. Gilje <jgilje@jgilje.net>
2016-12-04 12:56:59 +0100

    simplified

--- jgilje/luke4.cpp
+++ jgilje/luke4.cpp
@@ -3,37 +3,27 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<uint64_t*> process(std::vector<uint64_t*>& in) {
-    std::vector<uint64_t*> out;
+bool is_special(uint64_t i) {
+    if (std::to_string(i).find('7') != std::string::npos) {
+        return true;
+    } else if ((i % 7) == 0) {
+        return  true;
+    }
 
-    for (auto i : in) {
-        auto s = std::to_string(*i);
-        auto it = std::find(s.cbegin(), s.cend(), '7');
-        if (it != s.cend()) {
-            out.emplace_back(i);
-            *i = out.size();
-        } else if ((*i % 7) == 0) {
-            out.emplace_back(i);
-            *i = out.size();
+    return false;
+}
+
+int main() {
+    std::vector<uint64_t> numbers;
+
+    for (uint64_t i = 1, j = 0; i <= 1337; i++) {
+        if (is_special(i)) {
+            numbers.emplace_back(numbers[j++]);
+        } else {
+            numbers.emplace_back(i);
         }
     }
 
-    return out;
-}
-
-int main(int argc, char* argv[]) {
-    std::vector<uint64_t> numbers(1400);
-    std::vector<uint64_t*> working_vector(numbers.size());
-
-    for (uint64_t i = 0; i < numbers.size(); i++) {
-        numbers[i] = i + 1;
-        working_vector[i] = &numbers[i];
-    }
-
-    while (working_vector.size() > 0) {
-        working_vector = process(working_vector);
-    }
-
-    std::cout << numbers[1336] << std::endl;
+    std::cout << numbers.back() << std::endl;
     return 0;
 }
