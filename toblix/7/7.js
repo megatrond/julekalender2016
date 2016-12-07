688e1cfd8b5cb3e4b6fd541f00c55f810c2b334f
toblix <tgdahle@gmail.com>
2016-12-07 13:54:02 +0100

    Luke 7

--- toblix/7/7.js
+++ toblix/7/7.js
@@ -1,36 +1,16 @@
-var stream = require('stream');
-var fs = require('fs');
+const readline = require('readline');
+const fs = require('fs');
 
-var liner = new stream.Transform( { objectMode: true } );
-liner._transform = function (chunk, encoding, done) {
-     var data = chunk.toString();
-     if (this._lastLineData) data = this._lastLineData + data;
- 
-     var lines = data.split('\n');
-     this._lastLineData = lines.splice(lines.length-1,1)[0];
- 
-     lines.forEach(this.push.bind(this));
-     done();
-};
-liner._flush = function (done) {
-     if (this._lastLineData) this.push(this._lastLineData);
-     this._lastLineData = null;
-     done();
-};
- 
-var source = fs.createReadStream('./data.txt');
-source.pipe(liner);
-
-liner.on('readable', function () {
-     var line;
-     while (line = liner.read()) {
-		 check(line);
-     }
+const rl = readline.createInterface({
+	input: fs.createReadStream('data.txt')
+});
+rl.on('line', function(line) {
+ check(line);
 });
 
 let n = 0, w = 0;
 
-source.on('end', function() {
+rl.on('close', () => {
 	console.log(n + ',' + w);
 });
 
