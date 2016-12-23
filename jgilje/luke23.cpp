41f926277dc9d1989a39f81080d9dad4ecf3f54c
Joakim L. Gilje <jgilje@jgilje.net>
2016-12-23 10:50:06 +0100

    no dynamic alloc

--- jgilje/luke23.cpp
+++ jgilje/luke23.cpp
@@ -9,8 +9,7 @@ boost::multiprecision::cpp_int iterate(uint32_t steps, uint32_t options) {
     res[1] = 2;
     res[2] = 4;
 
-    for (uint32_t i = 3; i <= steps; i++) {
-        res.emplace_back(0);
+    for (uint32_t i = 3; i < steps; i++) {
         for (uint32_t j = 1; j <= options; j++) {
             res[i] = res[i] + res[i - j];
         }
